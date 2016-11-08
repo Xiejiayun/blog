1b316d8aaa135e5fcffb8b21eb56ecf58381bd15
Jeremy <furnace09@gmail.com>
2016-11-08 21:01:29 +0800

    implements add comment and delete comment

---
+++ blog/CommentCell.h
@@ -0,0 +1,13 @@
+//
+//  CommentCell.h
+//  blog
+//
+//  Created by Jiayun Xie on 16/11/8.
+//  Copyright © 2016年 Jiayun Xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentCell : UITableViewCell
+
+@end
---
+++ blog/CommentCell.m
@@ -0,0 +1,24 @@
+//
+//  CommentCell.m
+//  blog
+//
+//  Created by Jiayun Xie on 16/11/8.
+//  Copyright © 2016年 Jiayun Xie. All rights reserved.
+//
+
+#import "CommentCell.h"
+
+@implementation CommentCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
