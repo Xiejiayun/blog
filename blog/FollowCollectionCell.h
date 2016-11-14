729b8aca9169699b43aef932c01d9e7fc6462b6d
Jeremy <furnace09@gmail.com>
2016-11-14 09:14:36 +0800

    add swipe gesture to refresh blog

---
+++ blog/FollowCollectionCell.h
@@ -0,0 +1,13 @@
+//
+//  FollowCollectionCell.h
+//  blog
+//
+//  Created by Jiayun Xie on 16/11/9.
+//  Copyright © 2016年 Jiayun Xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FollowCollectionCell : UICollectionViewCell
+
+@end
---
+++ blog/SelfInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelfInfoViewController.h
+//  blog
+//
+//  Created by Jiayun Xie on 16/11/9.
+//  Copyright © 2016年 Jiayun Xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelfInfoViewController : UIViewController
+
+@end
